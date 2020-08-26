3cf17ce9ad9f7fd9606225476a81d9f398bfbb1f
PedroAntoniosalas <isc.pedroantonio@gmail.com>
2020-08-25 23:02:33 -0500

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,24 +1,26 @@
-<?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "test";
+<?php if (! defined ('BASEPATH')) exit ('No direct script access allowed');
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-if (!$conn)
-{
-    die ("No hay conexión:".mysqli_connect_error ());
+class Login extends CI_Controller {
+
+    public function_construct()
+    {
+        parent::_contruct();
+    }
+
+    public function index()
+    {
+        if (isset ($_POST['password'])){
+            $this->load->model('usuario_model');
+            if($this->usuario_model->login($_POST['username'],md5($_POST['password']))){
+                redirect('welcome');
+            } else{
+                redirect('login#bad-password');
+            }
+        }
+        $this->load->view('login')
+    }
 }
-$nombre = $_POST["txtusuario"];
-$pass =$_POST ["txtContraseña"];
 
- $query = mysqli_query($conn, "SELECT * FROM test  WHERE usuario = '".$nombre"' and password =  '".$pass"'");
- $nr = mysqli_query_rows ($query);
-
- if ($nr == 1)
- {
-     header("Location: login.html")
- }
 
 
 ?>
